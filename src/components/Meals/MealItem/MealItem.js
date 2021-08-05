a855ac292eedec1999e62129c10f25fb5924b3d3
Patricia Nobre <2108le@gmail.com>
2021-08-05 18:16:00 -0300

    coments on refs

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -5,10 +5,20 @@ import classes from './MealItem.module.css';
 
 // cria o componente MealItem
 const MealItem = (props) => {
+
+    // acessa o metodo context do react
     const cartCtx = useContext(CartContext);
+
+
     const price = `$${props.price.toFixed(2)}`;
 
+    // armazena o objeto que deve ser passado para a funçao onAddToCart
+    // onAddToCart deve ser passado como prop do componente MealItem form
+    // addToCartHandler serve como pointer para a prop onAddToCart
     const addToCartHandler = amount => {
+
+        // chama o metodo  addItem que definimos no nosso context
+        // encaminha o item  do context para o reducer que vai gerenciar os states do cart
         cartCtx.addItem({
             id: props.id,
             name: props.name,
@@ -18,16 +28,16 @@ const MealItem = (props) => {
     };
 
     //retorna o HTML/JSX
-    return(
+    return (
         <li className={classes.meal} >
-           <div>
-               <h3>{props.name}</h3>
-               <div className={classes.description}>{props.description}</div>
-               <div className={classes.price}>{price}</div>
-            </div> 
-           <div>
-               <MealItemForm onAddToCart={addToCartHandler} id={props.id} />
-           </div>
+            <div>
+                <h3>{props.name}</h3>
+                <div className={classes.description}>{props.description}</div>
+                <div className={classes.price}>{price}</div>
+            </div>
+            <div>
+                <MealItemForm onAddToCart={addToCartHandler} id={props.id} />
+            </div>
         </li>
     );
 };
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -3,28 +3,58 @@ import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 //cria o componente MealItemForm
+// é daqui que chamamos a constante addItemToCartHandler que está no componente CartProvider
 const MealItemForm = (props) => {
+    
+    // useState é usado para controlar quando o formulário é válido ou não
+    // inicialmente o state é true
+    // extraimos os valores de useState atraves do array com 2 elementos, uma constante que armazena o state e uma funçao que manipula o novo state
     const [amountIsValid, setAmountIsValid] = useState(true);
+    
+    // usamos refs no componente Input para extrair o amount que foi imputado
+    // no componente input, passamos o ref para a prop ref
+    // mas como é um componente customizado, precisamos ir até o compunente customizado input e importar o react
+    // e ter a funçao do componente através do React.forwardRef
+    // e passar como parametro da funcao React.forwardRef props e ref
+    // precisamos também setar a prop ref no <input ref ={ref}>
+    // só assim podemos ter acesso ao input atraves de refs
+    // e ler o valor imputado na função submitHandler
     const amountInputRef = useRef();
     
+    // dessa função obtemos o evento que é passado como parametro pelo onSubmite que fica no form
     const submitHandler = event => {
+        // chamamos o event.preventDefault para evitar que o browser automaticamente recarregue a página
         event.preventDefault();
+        
+        // enteredAmount armazena o valor imputado usando ref
+        // como padrao o valor é uma string
         const enteredAmount = amountInputRef.current.value;
+        
+        // força o valor a se tornar numero
         const enteredAmountNumber = +enteredAmount;
 
+        // validações
+        // verifica se nenhum texto foi imputado ou se o número é menor que 1 ou maior que 5
         if (
             enteredAmount.trim().length === 0 || 
             enteredAmountNumber < 1 || 
             enteredAmountNumber > 5
         ) {
+            // define o setAmountIsValid como false ao encontrar alguma das condiçoes acima
             setAmountIsValid(false);
+            
+            // se alguma das condiçoes acima forem encontradas, o processamento da função é parado
+            // podemos adicionar uma mensagem de erro e para isso temos que manipular o state usando o useState
             return;
         }
 
+        // passa através de props uma função para adicionar um item ao cart
+        // poderia ser feito  chamando o metodo context, mas como é uma situaçao de gerenciamento simples, pode ser feita por props
+        // essa função vai ser definida no componente MealItem
         props.onAddToCart(enteredAmountNumber);
     };
 
-    // retorna o HTML e o JSX
+    // retorna o HTML e o JSX se o conteudo impputado passou pels validaçoes acima
     return(
         <form className={classes.form} onSubmit={submitHandler} >
             <Input 
@@ -40,7 +70,9 @@ const MealItemForm = (props) => {
             }}
             />
             <button>+ Add</button>
-            {! amountIsValid && <p>Please enter a valid amount (1-5).</p> }
+            {
+            // conteudo renderizado condicionalmente
+            ! amountIsValid && <p>Please enter a valid amount (1-5).</p> }
         </form>
     );
 };
