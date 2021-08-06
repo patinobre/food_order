b947a485a6e799d100149b4941b38fecca868c06
Patricia Nobre <2108le@gmail.com>
2021-08-06 12:27:48 -0300

    outputting cart items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,46 @@
+import { useContext } from 'react';
 import Modal from '../UI/Modal';
+import CartItem from './CartItem';
 import classes from './Cart.module.css';
+import CartContext from '../../store/cart-context';
 
-// cria o componente Cart
+// cria o componente Cart que renderiza os cartItems
 const Cart = (props) => {
+    
+    // armazena as infos do cartContext
+    const cartCtx = useContext(CartContext);
+
+    // acessa o totalAmount do cartContext atraves do cartCtx e define 2 casas decimais através do toFixed(2)
+    // essa informaçao é inserida num template literal `` para adicionar o dollar sign $
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+
+    // verifica se o cartCtz não está vazio
+    const hasItems = cartCtx.items.length > 0;
+
+    // funçao que remove items do cart vai ser passada ao componente cartItem com pointer dentro da props onRemove
+    const cartItemRemoveHandler = id => { };
+
+    // funçao que adiciona items ao cart e vai ser passada ao componente cartItem com pointer dentro da props onAdd
+    const cartItemAddHandler = item => { };
+
     const cartItems = (
         <ul clasName={classes[`cart-items`]}>
-            {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
-                <li>{item.name}</li>
+            {
+            // rederiza os items do cartContext através do cartCtx
+            cartCtx.items.map((item) => (
+                // chama o componente CartItem e passa as props
+                <CartItem
+                    key={item.id}
+                    name={item.name}
+                    amount={item.amount}
+                    price={item.price}
+                    onRemove={
+                        // usamos o metodo bind para assegurar que o id do item a ser removido foi passado
+                        cartItemRemoveHandler.bind(null, item.id)}
+                    onAdd={
+                        // usamos o metodo bind para assegurar que todo o  item foi passado
+                        cartItemAddHandler.bind(null, item)}
+                />
             ))}
         </ul>
     );
@@ -17,11 +51,15 @@ const Cart = (props) => {
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
-                <span>35.62</span>
+                <span>{
+                // pointer para a funçao totalAmount
+                totalAmount}</span>
             </div>
             <div className={classes.actions}>
                 <button className={classes[`button--alt`]} onClick={props.onClose}>Close</button>
-                <button className={classes.button}>Order</button>
+                {
+                // rederiza condicionalmente o botao order
+                hasItems && <button className={classes.button}>Order</button>}
             </div>
         </Modal>
     );
