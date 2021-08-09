7ffe2e279d62aaae91c650a5ebbf0211feafa4b1
Patricia Nobre <2108le@gmail.com>
2021-08-09 12:48:47 -0300

    making items removable

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,40 +6,29 @@ import CartContext from '../../store/cart-context';
 
 // cria o componente Cart que renderiza os cartItems
 const Cart = (props) => {
-    
-    // armazena as infos do cartContext
     const cartCtx = useContext(CartContext);
 
-    // acessa o totalAmount do cartContext atraves do cartCtx e define 2 casas decimais através do toFixed(2)
-    // essa informaçao é inserida num template literal `` para adicionar o dollar sign $
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
-
-    // verifica se o cartCtz não está vazio
     const hasItems = cartCtx.items.length > 0;
 
-    // funçao que remove items do cart vai ser passada ao componente cartItem com pointer dentro da props onRemove
-    const cartItemRemoveHandler = id => { };
+    const cartItemRemoveHandler = id => {
+        cartCtx.removeItem(id);
+     };
 
-    // funçao que adiciona items ao cart e vai ser passada ao componente cartItem com pointer dentro da props onAdd
-    const cartItemAddHandler = item => { };
+    const cartItemAddHandler = item => {
+        cartCtx.addItem({...item, amount: 1});
+     };
 
     const cartItems = (
         <ul clasName={classes[`cart-items`]}>
-            {
-            // rederiza os items do cartContext através do cartCtx
-            cartCtx.items.map((item) => (
-                // chama o componente CartItem e passa as props
+            {cartCtx.items.map((item) => (
                 <CartItem
                     key={item.id}
                     name={item.name}
                     amount={item.amount}
                     price={item.price}
-                    onRemove={
-                        // usamos o metodo bind para assegurar que o id do item a ser removido foi passado
-                        cartItemRemoveHandler.bind(null, item.id)}
-                    onAdd={
-                        // usamos o metodo bind para assegurar que todo o  item foi passado
-                        cartItemAddHandler.bind(null, item)}
+                    onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                    onAdd={cartItemAddHandler.bind(null, item)}
                 />
             ))}
         </ul>
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -7,20 +7,21 @@ const defaultCartState = {
 };
 
 const cartReducer = (state, action) => {
-    if(action.type === 'ADD') {
+    if (action.type === 'ADD') {
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
-        
-        const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id
+
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.item.id
         );
-        
+
         const existingCartItem = state.items[existingCartItemIndex];
-        let updatedItem; 
-        let updatedItems; 
+        let updatedItem;
+        let updatedItems;
 
         if (existingCartItem) {
             updatedItem = {
-              ...existingCartItem,
-              amount: existingCartItem.amount + action.item.amount
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
             };
             updatedItems = [...state.items];
             updatedItems[existingCartItemIndex] = updatedItem;
@@ -29,41 +30,47 @@ const cartReducer = (state, action) => {
             updatedItems = state.items.concat(updatedItem);
         }
 
-        
-       
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
         };
     }
+
+    if (action.type === 'REMOVE') {
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.id
+        );
+        const existingItem = state.items[existingCartItemIndex];
+        const updatedTotalAmount = state.totalAmount - existingItem.price;
+        let updatedItems;
+        if (existingItem.amount === 1) {
+            updatedItems = state.items.filter(item => item.id !== action.id);
+        } else {
+            const updatedItem = {...existingItem, amount: existingItem.amount - 1};
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem
+        }
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+    };
+
     return defaultCartState;
 };
 
 const CartProvider = (props) => {
-    
-    // constante que armazena a funçao reducer
-    // passamos como elemento do array para armazenar state o cartSate 
-    // passamos como elemento do array para armazenar açao o dispatchCartAction
     const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
-    // quando essa função é chamada, obtemos o item que deve ser adicionado ao Cart
-    // precisamos checar se o item já está no carrinho ou se é um novo item
-    // na açao dispatch encaminhamos o item que deve ser adicionado para a função reducer
     const addItemToCartHandler = (item) => {
-        
-        //açao que dispara a açao de adicionar items no cart
-        dispatchCartAction({type: 'ADD', item: item});
+        dispatchCartAction({ type: 'ADD', item: item });
     };
-    
-    // quando essa função é chamada, obtemos o item que deve ser removido do Cart
-   const removeItemFromCartHandler = (id) => {
-        
-        //açao que dispara a açao de remover items no cart
-        dispatchCartAction({type: 'REMOVE', id: id});
+
+    const removeItemFromCartHandler = (id) => {
+        dispatchCartAction({ type: 'REMOVE', id: id });
     };
-    
-    // o cartState é usado para construir o objeto cartContext
-    // usamos a porpriedade items e a porpriedade totalAmount para aceesar as propriedades do cartState
+
     const cartContext = {
         items: cartState.items,
         totalAmount: cartState.totalAmount,
@@ -71,7 +78,7 @@ const CartProvider = (props) => {
         removeItem: removeItemFromCartHandler
     };
 
-    return(
+    return (
         <CartContext.Provider value={cartContext}>
             {props.children}
         </CartContext.Provider>
